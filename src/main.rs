eb1677de7cae7c5c0708a49cc1cfd336a83e3bd9
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2024-02-05 17:06:02 -0500

    Added basic networking capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io::stdin; // for user input
+use std::io::{stdin, Read, Write}; // for user input
+use std::net::{TcpListener, TcpStream}; // for networking
 
 // Generates a text menu for the user to select between choices by
 // inputting a number from 0 to choices.len()-1
@@ -15,8 +16,8 @@ fn menu_input(prompt: String, choices: Vec<String>) -> usize {
         stdin().read_line(&mut input).unwrap();
         match input.trim().parse::<i32>() {
             Ok(n) => {
-                if 0 <= n && n < choices.len().try_into().unwrap() {
-                    return n.try_into().unwrap();
+                if 0 <= n && n < choices.len() as i32 {
+                    return n as usize;
                 } else {
                     println!("\nMake sure your input is one of the choices.");
                 }
@@ -28,9 +29,56 @@ fn menu_input(prompt: String, choices: Vec<String>) -> usize {
     }
 }
 
+enum Role {
+    Server = 0,
+    Client = 1,
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).expect("Failed to read from client");
+    let request = String::from_utf8_lossy(&buffer[..]);
+    println!("Received request: {}", request);
+
+    let response = "Hello, client!".as_bytes();
+    stream.write(response).expect("Failed to write response to client");
+}
+
 fn main() {
-    let choice = menu_input("Select Type:".to_string(), 
-        vec!["Server".to_string(), "Client".to_string()]
-    );
-    println!("You chose {}.", choice);
+    // Getting the choice of role from the user, either server or client
+    let role: Role = match menu_input("Select Type:".to_string(), 
+    vec!["Server".to_string(), "Client".to_string()]) {
+        0 => Role::Server,
+        _ => Role::Client
+    };
+    
+    match role {
+        Role::Server => {
+            let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind to address");
+            println!("Server listening on 127.0.0.1:8080");
+
+            for stream in listener.incoming() {
+                match stream {
+                    Ok(stream) => { std::thread::spawn(|| handle_client(stream)); },
+                    Err(e) => { eprintln!("Failed to establish connection: {}", e); },
+                }
+            }
+        },
+        Role::Client => {
+            if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8080") {
+                println!("Connected to the server!");
+                // Send to server
+                stream.write("Hello, Server!".as_bytes()).expect("Could not write to stream.");
+                // Recieve response from server
+                let mut buffer = [0; 1024];
+                stream.read(&mut buffer).expect("Failed to read from client");
+                let response = String::from_utf8_lossy(&buffer[..]);
+                println!("Received response: {}", response);
+                let mut input = String::new();
+                stdin().read_line(&mut input).unwrap();
+            } else {
+                println!("Couldn't connect to server.");
+            }
+        }
+    }
 }
