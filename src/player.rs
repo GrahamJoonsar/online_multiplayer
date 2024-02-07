5adde032898d6dfeb5a7c35433118136a3e54eca
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2024-02-06 19:07:01 -0500

    Added the basic framework for the player and card classes

--- src/player.rs
+++ src/player.rs
@@ -1,3 +1,26 @@
+// The player is identified with a unique integer ID
+// they have cards in their hand, and cards in play on their side
 pub struct Player {
     id: i32,
+    hand: Vec<Card>,
+    side: Vec<(Card, Card)>,
+}
+
+// Lists all of the different card types
+enum CardType {
+    Soldier,
+    Barrier,
+    Bribe,
+    Mortar,
+    Empty,
+}
+
+pub struct Card {
+    card_type: CardType,
+}
+
+impl Card {
+    fn play(&self, player: &mut Player) {
+
+    }
 }
