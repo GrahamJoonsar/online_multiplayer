ee278ed72552bdb954ba69a9605536bbc006b74a
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2024-02-13 16:59:15 -0500

    Added an actually good menu with a library

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 // External Libs
-use std::io::{stdin, Read, Write}; // for user input
+use terminal_menu::{menu, label, button, run, mut_menu}; // For user input
 
 use game::GameState;
 
 // Internal Libs
 mod game;
-mod input;
+
+#[derive(Debug)]
+enum UserType {
+    Host,
+    Client,
+}
 
 fn main() {
-    let mut state: GameState = GameState::new(2);
+    let main_menu = menu(vec![
+        label("----------------"),
+        label("Select User type"),
+        label("----------------"),
+
+        button("Host"),
+        button("Join")
+    ]); run(&main_menu);
+
+    
+    let user_type: UserType = match mut_menu(&main_menu).selected_item_name() {
+        "Host" => UserType::Host,
+        _ => UserType::Client,
+    };
+
+    let game_state: GameState = GameState::new(2);
 }
