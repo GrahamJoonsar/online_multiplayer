9af9314355d5ac208bbc25af0678daf2fb140768
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2024-02-13 16:23:29 -0500

    Implemented GameState partially

--- src/game.rs
+++ src/game.rs
@@ -2,29 +2,45 @@ pub struct GameState {
     pub players: Vec<Player>,
 }
 
+impl GameState {
+    pub fn new(player_count: usize) -> GameState {
+        let mut state: GameState = GameState { players: vec![] };
+        for i in 0..player_count {
+            state.players.push(Player::new(i));
+        }
+        return state;
+    }
+
+    pub fn to_packet(&self) -> [u8; 1024] {
+        let mut buf: [u8; 1024] = [0; 1024];
+        buf[0] = self.players.len() as u8;
+        
+        return buf;
+    }
+}
+
 // The player is identified with a unique integer ID
 // they have cards in their hand, and cards in play on their side
 pub struct Player {
-    id: i32,
+    id: usize,
     hand: Vec<Card>,
-    side: Vec<(Card, Card)>,
+    side: (u8, u8),
+}
+
+impl Player {
+    pub fn new(id: usize) -> Player {
+        Player {
+            id,
+            hand: vec![],
+            side: (0, 0),
+        }
+    }
 }
 
 // Lists all of the different card types
-enum CardType {
+enum Card {
     Soldier,
     Barrier,
     Bribe,
     Mortar,
-    Empty,
-}
-
-pub struct Card {
-    card_type: CardType,
-}
-
-impl Card {
-    fn play(&self, player: &mut Player) {
-        
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 // External Libs
 use std::io::{stdin, Read, Write}; // for user input
 
+use game::GameState;
+
 // Internal Libs
-use crate::game::Player;
 mod game;
 mod input;
 
 fn main() {
-    
+    let mut state: GameState = GameState::new(2);
 }
